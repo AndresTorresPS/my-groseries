5d6c80a5f122713682d64ffec144a41f2a882d82
AndresTorres <jaimetodu@hotmail.com>
2026-04-27 21:02:43 -0500

    Intento 1 de enrutar home

--- ng-app/src/app/app.routes.ts
+++ ng-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home/home'; // Importación automática
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Home }, // Ruta vacía = Pantalla de inicio
+  { path: '**', redirectTo: '' }          // Si el usuario escribe cualquier cosa, vuelve a home
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
